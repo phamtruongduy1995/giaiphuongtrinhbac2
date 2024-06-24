663444c00233570e65297ff12be52ffcf29842fb
SPC <SPC@23-LAB402>
2024-06-24 20:10:08 +0700

    Add project files.

---
+++ Giải Phương Trình Bậc Hai/Giải Phương Trình Bậc Hai.cpp
@@ -0,0 +1,33 @@
+﻿#include <iostream>
+#include <cmath>
+using namespace std;
+int main()
+{
+    cout << "Chuong trinh giai phuong trinh bat hai";
+    int a, b, c;
+    double x1, x2, delta;
+    cout << "Nhap a b c: \n";
+    cin >> a;
+    cin >> b;
+    cin >> c;
+    if (a == 0) {
+        if (b == 0) {
+            cout << "Phuong trinh vo nghiem";
+        }
+        else {
+            x1 = -c / b;
+            cout << "Phuong trinh co 1 nghiem " << x1 << "\n";
+        }
+    }
+    else {
+        delta = b * b - 4 * a * c;
+        if (delta < 0) {
+            cout << "Phuong trinh vo nghiem";
+        }
+        else {
+            x1 = (-b + sqrt(delta)) / (2 * a);
+            x2 = (-b - sqrt(delta)) / (2 * a);
+            cout << "Phuong trinh co hai nghiem\n" << x1 << "\n" << x2;
+        }
+    }
+}
